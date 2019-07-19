a8d5a8b4481e7c565ccaee50e3fa2fc3b59b9c91
Karolina Gorlewska <k.gorlewska@gmail.com>
2019-07-19 13:17:39 +0100

    Fetch api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    planets: [],
+    isLoaded: false
+  };
+
+  componentDidMount() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const url = 'https://dry-plains-91502.herokuapp.com/planets';
+
+    fetch(proxyUrl + url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          isLoaded: true,
+          planets: data
+        });
+        console.log(data);
+      })
+      .catch(console.log);
+  }
+
   render() {
-    return <h1>planets</h1>;
+    const { isLoaded, planets } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return <h1>planets</h1>;
+    }
   }
 }
 
